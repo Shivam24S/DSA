e01b7ccb41f2492433a935b53fa46bee37082079
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-01 11:07:23 +0530

    insertion sort

---
+++ sorting algorithms/insertion_sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+
+    int arr[]={8,9,4,3,1,5};
+    int n = 6;
+
+
+
+    for(int i =1;i<n;i++){
+
+        int curr = arr[i];
+
+        int j = i-1;
+
+
+        while(j>=0&& arr[j]>curr){
+
+            arr[j+1]=arr[j];
+            j--;
+
+        }
+
+        arr[j+1]=curr;
+
+
+        
+
+    }
+
+
+    cout << "sorted element are ";
+    for(int i =0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+
+    cout<<endl;
+
+
+
+
+}
