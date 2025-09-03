73ebb862b239b3f39d13c7d53290d50160694a23
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-03 09:53:13 +0530

    selection sort code added

--- sorting algorithms/slelection_sort.cpp
+++ sorting algorithms/slelection_sort.cpp
@@ -6,31 +6,39 @@
 
 
 // int main(){
-//     int arr[]={5,8,9,1,3,6};
-//     int  n=6;
 
+//     int arr[]={8,4,6,9,1,3};
+//     int n=6;
 
 //     for(int i=0;i<n-1;i++){
-//         int smallestIndex = i;
+
+//         int smallestIndex= i;
 
 //         for(int j=i+1;j<n;j++){
-//             if(arr[j]<arr[smallestIndex]){
+//             if(arr[j]< arr[smallestIndex]){
 //                 smallestIndex=j;
 //             }
 //         }
+//             // swap(arr[i],arr[smallestIndex]);
 
-//         int temp = arr[i];
-//         arr[i]=arr[smallestIndex];
-//         arr[smallestIndex]=temp;
+//             int temp = arr[i];
+//             arr[i]=arr[smallestIndex];
+//             arr[smallestIndex]=temp;
 //     }
-    
-//     cout << "sorted elements are ";
+
+
+//     cout<< "sorted elements are ";
 //     for(int i=0;i<n;i++){
 //         cout<<arr[i] << " ";
 //     }
 //     cout<<endl;
+
 // }
 
+
+
+
+// using  function 
 
 
 #include <iostream>
@@ -41,36 +49,40 @@ using namespace std;
 void selectionSort(int arr[],int n){
 
     for(int i=0;i<n-1;i++){
-        int smallestIndex = i;
+        int smallestIndex=i;
 
-        for(int j=i+1;j<n;j++){
+        for(int j = i+1;j<n;j++){
             if(arr[j]<arr[smallestIndex]){
                 smallestIndex=j;
             }
+            
         }
+
+        // swap(arr[i],arr[smallestIndex]);
 
         int temp = arr[i];
         arr[i]=arr[smallestIndex];
         arr[smallestIndex]=temp;
+
+
     }
-    
-    cout << "sorted elements are ";
+
+    cout<<"sorted elements are " ;
     for(int i=0;i<n;i++){
-        cout<<arr[i] << " ";
+        cout << arr[i] << " ";
     }
-    cout<<endl;
-
-
+    cout << endl;
 
 }
 
+
 int main(){
 
-    int arr[]={5,4,6,3,1,2};
+
+    int arr[]={8,4,9,3,1,2};
     int n=6;
 
     selectionSort(arr,n);
 
     return 0;
-
 }
