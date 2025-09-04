d8db158782a0afdfc646156e8922dfbb7dd9814b
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-04 05:47:09 +0530

    merge sort code added

---
+++ sorting algorithms/mergeSort.cpp
@@ -0,0 +1,90 @@
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+void merge(vector<int> &arr,int start,int mid,int end){
+
+    vector<int> temp;
+
+    int i= start, j = mid+1;
+
+
+    while(i<=mid && j<=end){
+
+        if(arr[i]<=arr[j]){
+            temp.push_back(arr[i]);
+            i++;
+
+        }else {
+            temp.push_back(arr[j]);
+            j++;
+        }
+
+    }
+
+    while(i<= mid){
+        temp.push_back(arr[i]);
+        i++;
+    }
+
+    while(j<=end){
+        temp.push_back(arr[j]);
+        j++;
+
+    }
+
+
+    for(int k=0;k<temp.size();k++){
+        arr[start+k]=temp[k];
+
+    }
+
+
+}
+
+
+
+
+
+void mergeSort(vector<int> &arr,int start,int end ){
+
+    if(start<end){
+
+        int mid = start + (end -start)/2;
+
+        // left half
+        mergeSort(arr,start,mid);
+
+
+        // right half
+
+        mergeSort(arr,mid+1,end);
+
+        merge(arr,start,mid,end);
+
+
+    }
+
+
+
+
+}
+
+
+int main(){
+
+    vector<int> arr={10,5,2,8,6,2,5,7,8,9,3,1};
+
+    mergeSort(arr,0,arr.size()-1);
+
+
+    cout << "sorted elements are ";
+    for(int x :arr) cout << x << " ";
+    cout<<endl;
+    
+    return 0;
+
+}
