94bcab7f0f881e159d543ba62288cab601550547
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-05 05:09:32 +0530

    merge sort

--- sorting algorithms/mergeSort.cpp
+++ sorting algorithms/mergeSort.cpp
@@ -1,63 +1,115 @@
 
+
+// #include <iostream>
+// #include <vector>
+
+// using namespace std;
+
+
+
+
+
+
+// void mergeSort(vector<int>&arr,int start,int end){
+
+//     int mid = start + (end-start)/2;
+
+//     if(start<end){
+
+//         // lefthalf
+//         mergeSort(arr,start,mid);
+
+//         // right half
+
+//         mergeSort(arr,mid+1,end);
+
+
+
+
+//     }
+
+
+
+// }
+
+
+
+
+// int main(){
+
+//     vector<int> arr={5,8,4,6,9,1,2};
+
+//     mergeSort(arr,0,arr.size()-1);
+
+
+
+
+//     return 0;
+
+// }
+
+
+
+
+
+
+
+
+
+
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
 
-void merge(vector<int> &arr,int start,int mid,int end){
+void merge(vector<int>&arr,int start,int mid,int end){
 
-    vector<int> temp;
+    int i = start,j=mid+1;
 
-    int i= start, j = mid+1;
-
+    vector<int>temp;
 
     while(i<=mid && j<=end){
-
-        if(arr[i]<=arr[j]){
+        if(arr[i]<arr[j]){
             temp.push_back(arr[i]);
             i++;
-
         }else {
             temp.push_back(arr[j]);
             j++;
         }
-
     }
 
-    while(i<= mid){
-        temp.push_back(arr[i]);
+    while (i<=mid)
+    {
+       temp.push_back(arr[i]);
         i++;
     }
-
-    while(j<=end){
+    while (j<=end){
         temp.push_back(arr[j]);
         j++;
 
     }
-
+ 
 
     for(int k=0;k<temp.size();k++){
-        arr[start+k]=temp[k];
-
+        arr[start + k] = temp[k];
     }
+
 
 
 }
 
 
+void mergeSort(vector<int>&arr,int start,int end){
 
-
-
-void mergeSort(vector<int> &arr,int start,int end ){
+    int mid = start + (end-start)/2;
 
     if(start<end){
 
-        int mid = start + (end -start)/2;
 
         // left half
-        mergeSort(arr,start,mid);
 
+        mergeSort(arr,start,mid);
 
         // right half
 
@@ -66,6 +118,8 @@ void mergeSort(vector<int> &arr,int start,int end ){
         merge(arr,start,mid,end);
 
 
+
+
     }
 
 
@@ -74,17 +128,21 @@ void mergeSort(vector<int> &arr,int start,int end ){
 }
 
 
+
 int main(){
-
-    vector<int> arr={10,5,2,8,6,2,5,7,8,9,3,1};
-
-    mergeSort(arr,0,arr.size()-1);
+    vector<int> arr={4,9,21,1,8,5,6,2};
 
 
-    cout << "sorted elements are ";
-    for(int x :arr) cout << x << " ";
-    cout<<endl;
-    
+mergeSort(arr,0,arr.size()-1);
+
+
+cout << "sorted vector array elements are ";
+for(int i=0;i<arr.size();i++){
+    cout<<arr[i] << " ";
+}
+cout<<endl;
+
+
+
     return 0;
-
 }
