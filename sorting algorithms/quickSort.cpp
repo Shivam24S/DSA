f68770a2eaa7c2e5814ea48cfc603eacf3663633
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-10 06:55:33 +0530

    quick sort algo code added

--- sorting algorithms/quickSort.cpp
+++ sorting algorithms/quickSort.cpp
@@ -1,61 +1,45 @@
 
-
 #include <iostream>
+
 #include <vector>
 
 using namespace std;
 
 
+int partition (vector<int>&arr,int start,int end){
 
-int partition(vector<int>&arr,int start,int end){
+    int idx = start -1; int pivot = arr[end];
 
-
-    int idx = start -1;
-
-    int pivot = arr[end];
-
-
-    for(int j=start;j<end;j++){
+    for(int j =start;j<end;j++){
         if(arr[j]<=pivot){
             idx++;
-            swap(arr[j],arr[idx]);
-        }
+            // swap(arr[j],arr[idx]);
 
+            int temp = arr[j];
+            arr[j]=arr[idx];
+            arr[idx]=temp;
+        }
     }
 
     idx++;
     swap(arr[end],arr[idx]);
-
-
     return idx;
-
-
-
 
 
 }
 
 
-
-
-void quickSort(vector<int>&arr,int start, int end){
+void quickSort(vector<int>&arr,int start,int end){
 
     if(start<end){
 
-
         int pivIdx = partition(arr,start,end);
 
-        // left half
-        quickSort(arr,start,pivIdx -1);
+        quickSort(arr,start,pivIdx-1);
 
-        // right half
         quickSort(arr,pivIdx+1,end);
 
-
-
-
     }
-
 
 }
 
@@ -63,21 +47,21 @@ void quickSort(vector<int>&arr,int start, int end){
 
 
 int main(){
-    vector <int> arr = {8,9,1,6,2,3};
 
+    vector<int>arr = {8,7,9,5,1,2,3};
 
     quickSort(arr,0,arr.size()-1);
 
 
     cout << "sorted elements are ";
-    for( int i =0;i<arr.size();i++){
-        cout << arr[i] << " ";
-
+    for(int i=0;i<arr.size();i++){
+        cout << arr[i] <<  " ";
     }
-    cout<<endl;
+    cout << endl;
 
 
     return 0;
 
-
 }
+
+
