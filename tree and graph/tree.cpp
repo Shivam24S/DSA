8370824a5705c05a73bf35b6daa6b9c704828ab2
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-18 06:18:09 +0530

    tree  and tree traversal code added

---
+++ tree and graph/tree.cpp
@@ -0,0 +1,78 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value){
+        data = value;
+        right=left=nullptr;
+    }
+};
+
+
+class Tree{
+    public:
+    Node* root;
+
+    Tree(){
+        root = nullptr;
+    }
+    void createTree(){
+    root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left= new Node(4);
+    root->left->right = new Node(5);
+    
+}
+void preOrder(Node* node){
+    if(node == nullptr)return;
+    cout << node -> data << " ";
+    preOrder(node->left);
+    preOrder(node->right);
+};
+
+void inOrder(Node* node){
+    if(node == nullptr)return;
+    inOrder(node->left);
+    cout<<node->data << " ";
+    inOrder(node->right);
+};
+
+void postOrder(Node* node){
+    if(node == nullptr)return;
+    postOrder(node->left);
+    postOrder(node->right);
+    cout<<node ->data << " ";
+};
+};
+
+
+
+
+int main(){
+
+    Tree t;
+
+    t.createTree();
+
+    cout << "pre order: ";
+    t.preOrder(t.root);
+
+    cout << "\n in order: ";
+    t.inOrder(t.root);
+
+    cout << "\n Post order";
+    t.postOrder(t.root);
+
+    return 0;
+
+}
