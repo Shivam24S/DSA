a7675872715905036c9c7363562a8d55a1b51a93
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-18 06:47:45 +0530

    binary and tree related code added

--- tree and graph/tree.cpp
+++ tree and graph/tree.cpp
@@ -1,5 +1,88 @@
 
 
+// #include <iostream>
+
+// using namespace std;
+
+
+// class Node{
+//     public:
+//     int data;
+//     Node* left;
+//     Node* right;
+
+//     Node(int value){
+//         data = value;
+//         right=left=nullptr;
+//     }
+// };
+
+
+// class Tree{
+//     public:
+//     Node* root;
+
+//     Tree(){
+//         root = nullptr;
+//     }
+//     void createTree(){
+//     root = new Node(1);
+//     root->left = new Node(2);
+//     root->right = new Node(3);
+//     root->left->left= new Node(4);
+//     root->left->right = new Node(5);
+    
+// }
+// void preOrder(Node* node){
+//     if(node == nullptr)return;
+//     cout << node -> data << " ";
+//     preOrder(node->left);
+//     preOrder(node->right);
+// };
+
+// void inOrder(Node* node){
+//     if(node == nullptr)return;
+//     inOrder(node->left);
+//     cout<<node->data << " ";
+//     inOrder(node->right);
+// };
+
+// void postOrder(Node* node){
+//     if(node == nullptr)return;
+//     postOrder(node->left);
+//     postOrder(node->right);
+//     cout<<node ->data << " ";
+// };
+// };
+
+
+
+
+// int main(){
+
+//     Tree t;
+
+//     t.createTree();
+
+//     cout << "pre order: ";
+//     t.preOrder(t.root);
+
+//     cout << "\n in order: ";
+//     t.inOrder(t.root);
+
+//     cout << "\n Post order";
+//     t.postOrder(t.root);
+
+//     return 0;
+
+// }
+
+
+
+
+
+
+
 #include <iostream>
 
 using namespace std;
@@ -13,7 +96,7 @@ class Node{
 
     Node(int value){
         data = value;
-        right=left=nullptr;
+        left = right = nullptr;
     }
 };
 
@@ -25,54 +108,55 @@ class Tree{
     Tree(){
         root = nullptr;
     }
+
+
     void createTree(){
-    root = new Node(1);
-    root->left = new Node(2);
-    root->right = new Node(3);
-    root->left->left= new Node(4);
-    root->left->right = new Node(5);
-    
-}
-void preOrder(Node* node){
-    if(node == nullptr)return;
-    cout << node -> data << " ";
-    preOrder(node->left);
-    preOrder(node->right);
+        root = new Node(1);
+        root->left = new Node(2);
+        root->right = new Node(3);
+        root->right->right = new Node(4);
+        root->right->left = new Node(5);
+    }
+
+    void preOrder(Node * node){
+        if(node == nullptr) return;
+        cout<< node->data << " ";
+        preOrder(node->left);
+        preOrder(node->right);
+    }
+
+    void inOrder(Node* node){
+        if(node == nullptr)return;
+        inOrder(node->left);
+        cout<< node->data << " ";
+        inOrder(node->right);
+    }
+
+    void postOrder(Node* node){
+        if(node == nullptr)return;
+        postOrder(node->left);
+        postOrder(node->right);
+        cout<<node->data << " ";
+    }
+
+
+
 };
 
-void inOrder(Node* node){
-    if(node == nullptr)return;
-    inOrder(node->left);
-    cout<<node->data << " ";
-    inOrder(node->right);
-};
-
-void postOrder(Node* node){
-    if(node == nullptr)return;
-    postOrder(node->left);
-    postOrder(node->right);
-    cout<<node ->data << " ";
-};
-};
-
-
-
-
-int main(){
+int main (){
 
     Tree t;
 
     t.createTree();
 
-    cout << "pre order: ";
+    cout << "pre order ";
     t.preOrder(t.root);
 
-    cout << "\n in order: ";
+    cout << "\n in order";
     t.inOrder(t.root);
 
-    cout << "\n Post order";
+    cout << "\n post order";
     t.postOrder(t.root);
 
     return 0;
-
 }
