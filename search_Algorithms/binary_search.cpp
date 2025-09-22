e543abf8e31b5b4cde76633a0a13098894c4cac4
Shivam24S <shivamshrivastav8055@gmail.com>
2025-09-22 10:10:09 +0530

    tree traversal code added

--- search_Algorithms/binary_search.cpp
+++ search_Algorithms/binary_search.cpp
@@ -1,14 +1,75 @@
 
 
+// // // #include <iostream>
+
+// // // #include<vector>
+
+// // // using namespace std;
+
+
+
+// // // int  binarySearch(const vector<int>arr,int start,int end,int searchingValue){
+
+// // //     if(start<=end){
+
+// // //         int mid = start + (end- start)/2;
+
+
+// // //         if(arr[mid] == searchingValue){
+// // //             return mid;
+// // //         }else if ( arr[mid]<searchingValue){
+// // //             return binarySearch(arr,mid+1,end,searchingValue);
+// // //         }else {
+// // //             return binarySearch(arr,start,mid-1,searchingValue);
+// // //         }
+
+
+// // //     }
+// // //     return -1;
+
+// // // }
+
+
+
+
+// // // int main(){
+
+// // //     const vector<int>arr={1,2,3,4,5,6,7};
+
+// // //     int searchingValue=10;
+
+// // //     int idx = binarySearch(arr,0,arr.size()-1,searchingValue);
+
+
+// // //     if(idx == -1){
+// // //         cout << " element not found in array";
+// // //     }else {
+// // //         cout << searchingValue <<  " is at index position " << idx << endl;
+// // //     }
+
+// // //     return 0;
+
+// // // }
+
+
+
+// // // suppose if array is not sorted then how will you perform binary search
+
+
+
+
+
 // // #include <iostream>
 
 // // #include<vector>
+
+// // #include <algorithm>
 
 // // using namespace std;
 
 
 
-// // int  binarySearch(const vector<int>arr,int start,int end,int searchingValue){
+// // int  binarySearch( vector<int>arr,int start,int end,int searchingValue){
 
 // //     if(start<=end){
 
@@ -34,9 +95,13 @@
 
 // // int main(){
 
-// //     const vector<int>arr={1,2,3,4,5,6,7};
+// //      vector<int>arr={8,9,6,7,5,2,3,1};
 
-// //     int searchingValue=10;
+// //     //  1,2,3,5,6,7,8,9
+
+// //      sort(arr.begin(),arr.end());
+
+// //     int searchingValue=6;
 
 // //     int idx = binarySearch(arr,0,arr.size()-1,searchingValue);
 
@@ -53,37 +118,25 @@
 
 
 
-// // suppose if array is not sorted then how will you perform binary search
-
-
-
 
 
 // #include <iostream>
-
-// #include<vector>
-
-// #include <algorithm>
-
+// #include <vector>
 // using namespace std;
 
 
-
-// int  binarySearch( vector<int>arr,int start,int end,int searchingValue){
+// int binarySearch(const vector<int>&arr,int start,int end,int searchingValue){
 
 //     if(start<=end){
+//         int mid = start + (end-start)/2;
 
-//         int mid = start + (end- start)/2;
-
-
-//         if(arr[mid] == searchingValue){
+//         if(arr[mid]==searchingValue){
 //             return mid;
-//         }else if ( arr[mid]<searchingValue){
+//         }else if(arr[mid] <searchingValue){
 //             return binarySearch(arr,mid+1,end,searchingValue);
 //         }else {
 //             return binarySearch(arr,start,mid-1,searchingValue);
 //         }
-
 
 //     }
 //     return -1;
@@ -92,73 +145,77 @@
 
 
 
-
 // int main(){
+//     const vector<int>arr = {1,2,3,4,5,6};
 
-//      vector<int>arr={8,9,6,7,5,2,3,1};
-
-//     //  1,2,3,5,6,7,8,9
-
-//      sort(arr.begin(),arr.end());
-
-//     int searchingValue=6;
+//     int searchingValue=7;
 
 //     int idx = binarySearch(arr,0,arr.size()-1,searchingValue);
 
-
 //     if(idx == -1){
-//         cout << " element not found in array";
+//         cout << "no element matched found";
 //     }else {
-//         cout << searchingValue <<  " is at index position " << idx << endl;
+//         cout << searchingValue << " is at index position " << idx << endl; 
 //     }
 
-//     return 0;
 
+
+
+
+//     return 0;
 // }
 
 
 
 
-
 #include <iostream>
+
 #include <vector>
+
 using namespace std;
 
 
 int binarySearch(const vector<int>&arr,int start,int end,int searchingValue){
 
     if(start<=end){
-        int mid = start + (end-start)/2;
 
-        if(arr[mid]==searchingValue){
+        int mid = start +  (end -start) /2;
+
+        if(arr[mid] == searchingValue){
             return mid;
-        }else if(arr[mid] <searchingValue){
+        }else if (arr[mid]<searchingValue){
             return binarySearch(arr,mid+1,end,searchingValue);
         }else {
             return binarySearch(arr,start,mid-1,searchingValue);
         }
 
     }
+
     return -1;
 
 }
 
 
 
+
+
+
+
+
+
 int main(){
+
     const vector<int>arr = {1,2,3,4,5,6};
 
-    int searchingValue=7;
+    int searchingValue=8;
 
     int idx = binarySearch(arr,0,arr.size()-1,searchingValue);
 
     if(idx == -1){
-        cout << "no element matched found";
+        cout << "element not found" ;
     }else {
-        cout << searchingValue << " is at index position " << idx << endl; 
+        cout << searchingValue << " is at index position " << idx << endl;
     }
-
-
 
 
 
--- tree and graph/tree.cpp
+++ tree and graph/tree.cpp
@@ -1,87 +1,5 @@
 
 
-// #include <iostream>
-
-// using namespace std;
-
-
-// class Node{
-//     public:
-//     int data;
-//     Node* left;
-//     Node* right;
-
-//     Node(int value){
-//         data = value;
-//         right=left=nullptr;
-//     }
-// };
-
-
-// class Tree{
-//     public:
-//     Node* root;
-
-//     Tree(){
-//         root = nullptr;
-//     }
-//     void createTree(){
-//     root = new Node(1);
-//     root->left = new Node(2);
-//     root->right = new Node(3);
-//     root->left->left= new Node(4);
-//     root->left->right = new Node(5);
-    
-// }
-// void preOrder(Node* node){
-//     if(node == nullptr)return;
-//     cout << node -> data << " ";
-//     preOrder(node->left);
-//     preOrder(node->right);
-// };
-
-// void inOrder(Node* node){
-//     if(node == nullptr)return;
-//     inOrder(node->left);
-//     cout<<node->data << " ";
-//     inOrder(node->right);
-// };
-
-// void postOrder(Node* node){
-//     if(node == nullptr)return;
-//     postOrder(node->left);
-//     postOrder(node->right);
-//     cout<<node ->data << " ";
-// };
-// };
-
-
-
-
-// int main(){
-
-//     Tree t;
-
-//     t.createTree();
-
-//     cout << "pre order: ";
-//     t.preOrder(t.root);
-
-//     cout << "\n in order: ";
-//     t.inOrder(t.root);
-
-//     cout << "\n Post order";
-//     t.postOrder(t.root);
-
-//     return 0;
-
-// }
-
-
-
-
-
-
 
 #include <iostream>
 
@@ -91,12 +9,12 @@ using namespace std;
 class Node{
     public:
     int data;
-    Node* left;
     Node* right;
+    Node* left ;
 
     Node(int value){
         data = value;
-        left = right = nullptr;
+        right= left = nullptr;
     }
 };
 
@@ -106,23 +24,24 @@ class Tree{
     Node* root;
 
     Tree(){
-        root = nullptr;
+        root=nullptr;
     }
 
 
     void createTree(){
         root = new Node(1);
-        root->left = new Node(2);
-        root->right = new Node(3);
-        root->right->right = new Node(4);
-        root->right->left = new Node(5);
+        root->left= new Node(2);
+        root->right= new Node(3);
+        root->left->left = new Node(4);
+        root->left->right = new Node(5);
     }
 
-    void preOrder(Node * node){
-        if(node == nullptr) return;
-        cout<< node->data << " ";
+
+    void preOrder(Node* node){
+        if(node == nullptr)return;
+        cout << node->data << " ";
         preOrder(node->left);
-        preOrder(node->right);
+        preOrder(node->right); 
     }
 
     void inOrder(Node* node){
@@ -136,27 +55,39 @@ class Tree{
         if(node == nullptr)return;
         postOrder(node->left);
         postOrder(node->right);
-        cout<<node->data << " ";
+        cout << node->data << " ";
     }
 
 
 
+    
 };
 
-int main (){
+
+
+
+int main(){
 
     Tree t;
 
     t.createTree();
-
-    cout << "pre order ";
+    
+    cout<<"pre order ";
     t.preOrder(t.root);
+    cout << "\n";
 
-    cout << "\n in order";
+
+    cout<<"in order ";
     t.inOrder(t.root);
+    cout << "\n";
 
-    cout << "\n post order";
+
+    cout<<"post order ";
     t.postOrder(t.root);
+    cout << "\n";
+
+
+
 
     return 0;
 }
